bf7b96faba2b2c9a1fe0ec4aeee4a914b021f5a2
astraleuro <astral.euro@gmail.com>
2020-06-03 19:41:58 +0700

    newgui

---
+++ widgets/welcomescreen.cpp
@@ -0,0 +1,59 @@
+#include "welcomescreen.h"
+#include "ui_welcomescreen.h"
+
+WelcomeScreen::WelcomeScreen(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::WelcomeScreen)
+{
+    ui->setupUi(this);
+
+    connect(ui->createBase, SIGNAL(clicked()), this, SLOT(saveData()));
+    connect(ui->openBase, SIGNAL(clicked()), this, SLOT(openData()));
+    connect(ui->closeButton, SIGNAL(clicked()), this, SIGNAL(closeApp()));
+}
+
+WelcomeScreen::~WelcomeScreen()
+{
+    delete ui;
+}
+
+void WelcomeScreen::saveData()
+{
+    QJsonObject json;
+    QFileDialog dialog(nullptr, SAVEFILE_TITLE, "", BASE_EXTENSION_FILTER);
+    dialog.setDefaultSuffix(BASE_EXTENSION);
+    dialog.setFileMode(QFileDialog::AnyFile);
+    dialog.setAcceptMode(QFileDialog::AcceptSave);
+    dialog.setModal(true);
+    dialog.exec();
+    if (dialog.result() == QDialog::Accepted && !dialog.selectedFiles().isEmpty()) {
+        QString fn = dialog.selectedFiles()[0];
+        if (!fn.isEmpty())
+            if (checkPath(fn.left(fn.count() - QString(BASE_EXTENSION).count()), true)) {
+                json = initObject();
+                if (writeJson(fn, json))
+                    dataReady(fn, json);
+            }
+    }
+}
+
+void WelcomeScreen::openData()
+{
+    QJsonObject json;
+    QFileDialog dialog(nullptr, OPENFILE_TITLE, "", BASE_EXTENSION_FILTER);
+    dialog.setDefaultSuffix(BASE_EXTENSION);
+    dialog.setFileMode(QFileDialog::ExistingFile);
+    dialog.setAcceptMode(QFileDialog::AcceptOpen);
+    dialog.setModal(true);
+    dialog.exec();
+    if (dialog.result() == QDialog::Accepted && !dialog.selectedFiles().isEmpty()) {
+        QString fn = dialog.selectedFiles()[0];
+        if (!fn.isEmpty())
+            if (checkPath(fn) && checkPath(fn.left(fn.count() - QString(BASE_EXTENSION).count()))) {
+                QJsonParseError log;
+                if (readJson(fn, json, log))
+                    if (log.error == QJsonParseError::NoError)
+                        dataReady(fn, json);
+            }
+    }
+}
